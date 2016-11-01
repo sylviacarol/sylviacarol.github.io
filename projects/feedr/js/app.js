ff2cab9e6303b93df431c3406909837d9434bad3
sylviacarol <sylviacarol.jorge@gmail.com>
2016-11-01 16:58:02 -0400

    feedr homepage

--- projects/feedr/js/app.js
+++ projects/feedr/js/app.js
@@ -3,3 +3,23 @@
 */
 
 
+
+$(document).ready(function () {
+		
+	var mashableURL = 'https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json'
+
+
+	$.get(mashableURL, function(results) {
+
+		var source = $('#news-template').html();
+
+		var template = Handlebars.compile(source);
+
+		var newsTemplate = template(results['new']);
+
+		$('#news-output').append(newsTemplate);
+
+	}); // end get
+
+
+}); //end ready
