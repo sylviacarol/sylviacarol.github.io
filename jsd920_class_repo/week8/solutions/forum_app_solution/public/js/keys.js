9426e934509fcc37b306b195db1b947a2e8f9bdc
sylviacarol <sylviacarol.jorge@gmail.com>
2016-11-28 12:58:48 -0500

    repo update past classes content[B

---
+++ jsd920_class_repo/week8/solutions/forum_app_solution/public/js/keys.js
@@ -0,0 +1,17 @@
+// Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyDnvHoVZ3CtyZNywVwuQ1P72nylKnRZd58",
+//   authDomain: "jsd-test-1.firebaseapp.com",
+//   databaseURL: "https://jsd-test-1.firebaseio.com",
+//   storageBucket: "jsd-test-1.appspot.com",
+// };
+
+var config = {
+    apiKey: "AIzaSyAbcV2B9tL4ydEAXObrlrR4PAUDIfrzOlQ",
+    authDomain: "forum-4734a.firebaseapp.com",
+    databaseURL: "https://forum-4734a.firebaseio.com",
+    storageBucket: "forum-4734a.appspot.com",
+    messagingSenderId: "432724807335"
+ };
+
+firebase.initializeApp(config);
--- jsd920_class_repo/week9/class=17-deployment_google-maps/google_maps/js/app.js
+++ jsd920_class_repo/week9/class=17-deployment_google-maps/google_maps/js/app.js
@@ -1,4 +1,51 @@
 $(document).ready(function() {
-})
+
+	var myMap;
+	
+	function initMap() {
+		myMap = new google.maps.Map(document.getElementById('map'), {
+			center: { lat: 40.7127, lng: -74.0059},
+			zoom: 14
+		});
+	} //end initMap
+
+
+	initMap();
+
+	$.ajax({
+		url: 'http://api.citybik.es/citi-bike-nyc.json',
+		type:'GET',
+		success: function(data) {
+			buildBikeMarkers(data);
+		},
+		error: function(xhr) {
+			console.log(error);
+		}
+	});
+
+
+	function buildBikeMarkers(stations) {
+		var infoWindow = new google.maps.InfoWindow();
+		stations.forEach(function(station){
+			var lat = station.lat / 1000000;
+			var lng = station.lng / 1000000;
+			var marker = new google.maps.Marker({
+				position: {lat:lat, lng:lng},
+				map: myMap,
+				title: station.name
+			});
+
+			var content = '<div>' + station.name + '</div>';
+			marker.addListener('click', function(){
+				infoWindow.close();
+				infoWindow.setContent(content);
+				infoWindow.open(map,marker);
+			});
+
+		});
+	}
+
+
+}); // document.ready
 
 
