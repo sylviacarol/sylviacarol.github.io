5415cc9133bb449e65835587e5cf4e334ce8bf0a
sylviacarol <sylviacarol.jorge@gmail.com>
2016-11-29 18:30:38 -0500

    Added dynamic state

--- projects/final/fc-all/src/Card.js
+++ projects/final/fc-all/src/Card.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import './Card.css';
+import moment from 'moment';
 
 class Card extends Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	      modal: 'hidden'
+	    };
+	  } 
+  
 	createMarkup(content) {
 		return {__html: content};
+	}
+
+	showModal() {
+		 this.setState({modal: 'show'});
 	}
 
 	render() {
@@ -23,21 +35,27 @@ class Card extends Component {
 			primaryImage = <img className="article__thumb" role="presentation" src={primaryImage.originalSrc}/> 
 		}
 
+		let modalClass = 'modal';
+
+		if (this.state.modal === 'show') {
+			modalClass = 'modal show';
+		}
+
 		return (
 		<div className="Card">
 			{primaryImage}  
 
 			<div className="article__meta">
                 <a className="article__site-brand" href={parentSite.url} target="blank">{coSite}</a>
-                <time className="article__timestamp">{publishTimeHuman}</time>
+                <time className="article__timestamp">{moment(publishTimeHuman).format("MMM Do YY")}</time>
             </div>
 
 
-			<h3 className="article__headline">{title}</h3>
+			<h3 className="article__headline" onClick={this.showModal.bind(this)}>{title}</h3>
 
 
 
-			<div className="modal">
+			<div className={modalClass}>
 				<p dangerouslySetInnerHTML={this.createMarkup(content)} />;
 			</div>
 		 </div>
